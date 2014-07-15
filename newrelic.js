a97b10bed96b79aa86155150ada7ecdcda93959c
David Pavlik <davidpav@David-Pavliks-MacBook-Pro.local>
2014-07-15 16:50:23 +0200

    Adding newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Fotkich'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '44a4f05272103772eee7cd1e3c7cba1d7c1417e8',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
